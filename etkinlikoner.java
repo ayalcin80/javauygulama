1db443a000732cc7a64e5ee6b58069527fcb31a4
ayalcin80 <107173268+ayalcin80@users.noreply.github.com>
2022-06-13 14:32:12 +0300

    Add files via upload

---
+++ etkinlikoner.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class etkinlikoner {
+    public static void main(String[] args) {
+        int havaSicakligi;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hava sıcaklığını giriniz: ");
+        havaSicakligi = scanner.nextInt();
+
+//        if (havaSicakligi < 5) {
+//            System.out.println("Kayak yapabilirsiniz.");
+//        } else if (havaSicakligi <= 15) {
+//            System.out.println("Sinemaya gidebilirsiniz.");
+//        } else if (havaSicakligi <= 25) {
+//            System.out.println("Piknik yapabilirsiniz.");
+//        } else {
+//            System.out.println("Yüzmeye gidebilirsiniz.");
+//        }
+
+        String etkinlik = havaSicakligi < 5 ? "Kayak yapabilirsiniz." :
+                havaSicakligi <= 15 ? "Sinemaya gidebilirsiniz." :
+                        havaSicakligi <= 25 ? "Piknik yapabilirsiniz." :
+                                "Yüzmeye gidebilirsiniz.";
+
+        scanner.close();
+
+        System.out.println(etkinlik);
+
+    }
+}
